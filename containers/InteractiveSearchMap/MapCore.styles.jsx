a25406c71433ac2909e29659f399f8bb27e56186
Jacky Tung <jacky.tung@langlive.com>
2022-04-10 11:42:43 +0800

    feat: init interactive search map route

---
+++ containers/InteractiveSearchMap/MapCore.styles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledMapContainer = styled.div`
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+`;
+
+export const StyledProgress = styled.div`
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  background-color: red;
+`;
---
+++ pages/interactive-search-map.js
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+import InteractiveSearchMap from "@/containers/InteractiveSearchMap";
+
+const MapPage = () => {
+  return (
+    <Layout title="Interactive Search Map page">
+      <InteractiveSearchMap />
+    </Layout>
+  );
+};
+
+export default MapPage;
